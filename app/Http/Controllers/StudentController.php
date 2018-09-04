bc07aabc4f678df61f26c3a3d8a1913f954b5e48
Elmer Datolayta <e.datolaytz@gmail.com>
2018-09-04 12:47:46 +1000

    Updated List

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class StudentController extends Controller
+{
+    /**
+     * Retrieve the user for the given ID.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function getStudent()
+    {
+        return "Student List Here";
+    }
+}
